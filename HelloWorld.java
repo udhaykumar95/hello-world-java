7ecf73e0e28a0efff542f622f268cf825f986f4b
udhaykumar95 <115972405+udhaykumar95@users.noreply.github.com>
2022-12-27 16:16:16 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("this is my first java program");
+     System.out.println("Thank you");
   }
 }
