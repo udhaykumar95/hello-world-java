262cd5f128bd4548528305ca29ea7b5ddf34396e
udhaykumar95 <115972405+udhaykumar95@users.noreply.github.com>
2023-01-02 12:39:33 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
     System.out.println("Hello world!");
     System.out.println("this is my first java program");
      System.out.println("Thank you");
+    System.out.println("Have a nice day");
   }
 }
